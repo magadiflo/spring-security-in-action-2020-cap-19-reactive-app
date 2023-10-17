913f9cf880deea0b1b63389aef64fcb9e791b078
Martín <magadiflo@gmail.com>
2023-10-17 13:07:57 -0500

    Aplicando autorización en la capa endpoint en aplicaciones reactivas

--- src/main/java/com/magadiflo/reactive/app/config/ProjectConfig.java
+++ src/main/java/com/magadiflo/reactive/app/config/ProjectConfig.java
@@ -2,7 +2,8 @@ package com.magadiflo.reactive.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
@@ -12,6 +13,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalTime;
 
 @Configuration
 public class ProjectConfig {
@@ -21,7 +26,7 @@ public class ProjectConfig {
         UserDetails admin = User.builder()
                 .username("admin")
                 .password("12345")
-                .authorities("read")
+                .authorities("ROLE_ADMIN")
                 .build();
 
         return new MapReactiveUserDetailsService(admin);
@@ -34,11 +39,32 @@ public class ProjectConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http.authorizeExchange(exchanges -> exchanges
-                        .pathMatchers(HttpMethod.GET, "/api/v1/greetings/hello").authenticated()
-                        .anyExchange().permitAll()
+        return http.authorizeExchange(authorize -> authorize
+                        .anyExchange().access(this.getAuthorizationDecision())
                 )
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+    private ReactiveAuthorizationManager<AuthorizationContext> getAuthorizationDecision() {
+        return (authentication, context) -> {
+            String path = this.getRequestPath(context);
+            boolean restrictedTime = LocalTime.now().isAfter(LocalTime.NOON);
+
+            if (path.equals("/api/v1/greetings/hello")) {
+                return authentication
+                        .map(a -> a.getAuthorities().stream()
+                                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN")))
+                        .map(isAdmin -> isAdmin && !restrictedTime)
+                        .map(AuthorizationDecision::new);
+            }
+
+            return Mono.just(new AuthorizationDecision(false));
+        };
+    }
+
+    private String getRequestPath(AuthorizationContext context) {
+        return context.getExchange().getRequest().getPath().toString();
+    }
+
 }
