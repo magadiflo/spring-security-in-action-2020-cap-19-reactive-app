96d2d7decd89458d6e9e075eb333067304cf418f
Martín <magadiflo@gmail.com>
2023-10-17 10:51:10 -0500

    pathMatchers(), configurando regla de autorización

--- src/main/java/com/magadiflo/reactive/app/config/ProjectConfig.java
+++ src/main/java/com/magadiflo/reactive/app/config/ProjectConfig.java
@@ -2,17 +2,20 @@ package com.magadiflo.reactive.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
-    // Añade un ReactiveUserDetailsService al contexto Spring
     @Bean
     public ReactiveUserDetailsService reactiveUserDetailsService() {
         UserDetails admin = User.builder()
@@ -21,13 +24,21 @@ public class ProjectConfig {
                 .authorities("read")
                 .build();
 
-        // Crea un MapReactiveUserDetailsService para gestionar las instancias de UserDetails
         return new MapReactiveUserDetailsService(admin);
     }
 
-    // Agrega un PasswordEncoder al contexto de Spring
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.GET, "/api/v1/greetings/hello").authenticated()
+                        .anyExchange().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
--- src/main/java/com/magadiflo/reactive/app/controllers/HelloController.java
+++ src/main/java/com/magadiflo/reactive/app/controllers/HelloController.java
@@ -1,7 +1,6 @@
 package com.magadiflo.reactive.app.controllers;
 
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +11,13 @@ import reactor.core.publisher.Mono;
 public class HelloController {
 
     @GetMapping(path = "/hello")
-    public Mono<String> hello() {
-        return ReactiveSecurityContextHolder.getContext()
-                .map(SecurityContext::getAuthentication)
-                .map(authentication -> "Hola, " + authentication.getName());
+    public Mono<String> hello(Mono<Authentication> authenticationMono) {
+        return authenticationMono.map(authentication -> "Hola, " + authentication.getName());
+    }
+
+    @GetMapping(path = "/ciao")
+    public Mono<String> ciao() {
+        return Mono.just("Chao!");
     }
 
 }
